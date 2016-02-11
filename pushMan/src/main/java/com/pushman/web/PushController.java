e9508428638bc9a2ccb9a35a1ebd39a2238f01e1
yusik <yusik@DESKTOP-PHNFL1F>
2016-02-11 11:42:49 +0900

    DB modeling / push Controller

--- pushMan/src/main/java/com/pushman/web/PushController.java
+++ pushMan/src/main/java/com/pushman/web/PushController.java
@@ -18,6 +18,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.pushman.dao.AuthNumberDao;
 import com.pushman.domain.PushCampaignVo;
@@ -92,14 +93,26 @@ public class PushController {
 		jo.addProperty("msgType", "T");
 		jo.addProperty("pushTime", 1800);
 		jo.addProperty("pushTitle", pushPopupTitle);
-		jo.addProperty("popupContent", pushPopupContent);
-		jo.addProperty("pushMsg", "test_메세지??!!");
+		jo.addProperty("pushMsg", pushPopupContent);
 		jo.addProperty("inappContent", innerContent);
 		jo.addProperty("pushKey", "1");
 		jo.addProperty("pushValue", "http://www.pushpia.com");
 		jo.addProperty("reserveTime", "20150417101702");
-		jo.addProperty("reqUid", "pushpia_20150417101702");
-		jo.addProperty("custId", "436149");
+//		jo.addProperty("reqUid", "pushpia_20150417101702");
+//		jo.addProperty("custId", "436149");
+
+		JsonArray jarr = new JsonArray();
+		JsonObject jo1 = new JsonObject();
+		JsonObject jo2 = new JsonObject();
+		jo1.addProperty("reqUid", "pushpia_20150417101702");
+		jo1.addProperty("custId", "436149");
+		jo2.addProperty("reqUid", "pushpia_20150417101712");
+		jo2.addProperty("custId", "kohyusik");
+		jarr.add(jo1);
+		jarr.add(jo2);
+		
+		jo.add("list", jarr);
+		
 
 		try {
 			paramValue = URLEncoder.encode(jo.toString(), "UTF-8");
