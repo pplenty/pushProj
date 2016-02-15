5071a52559e41a2282492278077e15af5b0427ab
kohyusik <newtype26@naver.com>
2016-02-15 10:04:21 +0900

    1602151003 커밋

--- pushMan/src/main/java/com/pushman/service/UpdateEngine.java
+++ pushMan/src/main/java/com/pushman/service/UpdateEngine.java
@@ -1,13 +1,26 @@
 package com.pushman.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import com.pushman.dao.TB_SEND_QUE_LOG_Dao;
+import com.pushman.domain.TB_SEND_QUE_LOG_Vo;
+import com.pushman.util.MultipleDataSource;
+
 @Service
 public class UpdateEngine {
+	@Autowired
+	TB_SEND_QUE_LOG_Dao tbSendQueLogDao;
 	// 로그 스케줄러
-//	 @Scheduled(fixedDelay = 1000)
-//	 public void getPushLogSchedular() throws RuntimeException {
-//		 System.out.println("1zzzz");
-//	 }
+	 @Scheduled(fixedDelay = 5000)
+	 public void getPushLogSchedular() throws RuntimeException {
+		 
+		 MultipleDataSource.setDataSourceKey("pushpiaDB");
+		 List<TB_SEND_QUE_LOG_Vo> pushLogList = tbSendQueLogDao.selectListAll(null);
+		 System.out.println(pushLogList.size());
+	 }
+	
 }
