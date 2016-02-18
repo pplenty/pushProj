62d8c2572f8a9d92c53ceaf5fa451158ff0d633e
pridebj8 <pridebj88@gmail.com>
2016-02-18 19:48:32 +0900

    config.js

--- pushMan/src/main/webapp/editor/config.js
+++ pushMan/src/main/webapp/editor/config.js
@@ -12,19 +12,38 @@ CKEDITOR.editorConfig = function( config ) {
 	config.extraPlugins = 'wordcount,notification'; 
 	config.wordcount = {
 
-		     // Whether or not you want to show the Word Count
-		     showWordCount: false,
+		    // Whether or not you want to show the Paragraphs Count
+		    showParagraphs: true,
 
-		     // Whether or not you want to show the Char Count
-		     showCharCount: true,
-		     
-		     // Maximum allowed Word Count
-		     maxWordCount: 100,
+		    // Whether or not you want to show the Word Count
+		    showWordCount: true,
 
-		     // Maximum allowed Char Count
-		     maxCharCount: 3500
-		  
-		 };
+		    // Whether or not you want to show the Char Count
+		    showCharCount: false,
+
+		    // Whether or not you want to count Spaces as Chars
+		    countSpacesAsChars: false,
+
+		    // Whether or not to include Html chars in the Char Count
+		    countHTML: true,
+		    
+		    // Maximum allowed Word Count, -1 is default for unlimited
+		    maxWordCount: 100,
+
+		    // Maximum allowed Char Count, -1 is default for unlimited
+		    maxCharCount: 3500,
+
+		    // Add filter to add or remove element before counting (see CKEDITOR.htmlParser.filter), Default value : null (no filter)
+		    filter: new CKEDITOR.htmlParser.filter({
+		        elements: {
+		            div: function( element ) {
+		                if(element.attributes.class == 'mediaembed') {
+		                    return false;
+		                }
+		            }
+		        }
+		    })
+		};
 
 	// The toolbar groups arrangement, optimized for two toolbar rows.
 	config.toolbarGroups = [
