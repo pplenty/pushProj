72903eb14fcd2466e4f48a6810100a8a31148b4c
kohyusik <newtype26@naver.com>
2016-03-02 17:59:41 +0900

    .

--- pushMan/src/test/java/pushMan/test.java
+++ pushMan/src/test/java/pushMan/test.java
@@ -1,10 +1,41 @@
 package pushMan;
 
+import java.util.ArrayList;
+
 public class test {
 
 
 	public static void main(String[] args) {
-		
+//		System.out.println(divisibilityOfFactorials(10));
+//		System.out.println(divisibilityOfFactorials(25));
+		System.out.println(divisibilityOfFactorials(2000));
 	}
 
+	
+	static int divisibilityOfFactorials(int N) {
+		
+		if (N == 2) return N;
+		boolean isPrime = true;
+
+
+		for (int i = 2; i < N; i++) {
+			if (N % i == 0) {
+				isPrime = false;
+				break;
+			}
+			continue;
+		}
+
+		if (isPrime == true)
+			return N;
+		
+		
+		
+		
+		
+		return 0;
+	}
+	
+	
+	
 }
